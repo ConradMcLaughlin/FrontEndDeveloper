6f43788d8acb2a32b4828e7278678cc502ff551b
Conrad McLaughlin <conrad.mclaughlin@outlook.com>
2017-09-15 16:53:25 +0100

    New HTML5 web page and JavaScript. Audio, Video, WebStorage API & Geolocation API examples.

---
+++ FrontEndDeveloper/js/HTML5.js
@@ -0,0 +1,16 @@
+﻿
+
+$(document).ready(function () {
+    
+    "use strict";
+
+    /*
+    //  With HTML5 you can use web storage: 
+    //  sessionStorage - destroyed once user closes browser
+    //  localStorage - stores data with no expiration date
+    localStorage.setItem("key1", "value1")
+    alert(localStorage.getItem("key1"));
+    localStorage.removeItem("key1");
+    localStorage.clear();  
+    */
+})
